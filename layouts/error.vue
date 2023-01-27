dc2db2a92cd49d2fe6af48f233eb291dd04418a8
abrosimov <abrosimov@tdme.ru>
2023-01-27 15:52:30 +0300

    edit blog model

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class='dark:bg-zinc-900 dark:text-zinc-200'>
+    <client-only>
+      <div class='container w-full'>
+        <h1 class='font-bold text-4xl text-center'>Ошибка 404</h1>
+        <h2 class='font-bold text-3xl text-center'>Страница не найдена</h2>
+        <h3 class='font-bold text-2xl text-center'>Вернитесь на <nuxt-link class='underline hover:no-underline hover:text-primary' to='/'>Главную страницу</nuxt-link></h3>
+      </div>
+    </client-only>
+  </div>
+</template>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,8 +1,14 @@
-export default function ({ $axios }, inject) {
+export default function ({ $axios, redirect }, inject) {
 
 
   const api = $axios.create({
     baseURL: 'https://za-halyavoi.ru/api'
+  })
+
+  api.onError(error => {
+    if (error.response.status === 404) {
+      redirect("/404")
+    }
   })
 
   inject('api', api)
