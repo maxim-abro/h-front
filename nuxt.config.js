21acd0ebc8472c2903a3e196c4b515892426f59d
abrosimov <abrosimov@tdme.ru>
2023-02-10 10:09:21 +0300

    edit title template

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: '',
-    titleTemplate: '%s - za-halyavoi.ru',
+    titleTemplate: '%s',
     htmlAttrs: {
       lang: 'ru'
     },
